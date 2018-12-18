3b0593e3129769afc2e58d49f18f96849637c667
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2018-12-18 16:52:45 +0100

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,13 @@
+# Load Tomcat Native Library
+#LD_LIBRARY_PATH=/opt/alfresco/common/lib:$LD_LIBRARY_PATH
+
+JAVA_HOME=/opt/alfresco/java
+JRE_HOME=$JAVA_HOME
+JAVA_OPTS="-XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Djava.awt.headless=true -Dalfresco.home=/opt/alfresco -XX:ReservedCodeCacheSize=128m $JAVA_OPTS "
+JAVA_OPTS="-Dcom.sun.management.jmxremote -Dsun.security.ssl.allowUnsafeRenegotiation=true $JAVA_OPTS "
+JAVA_OPTS="-XX:NewRatio=2 -XX:+CMSParallelRemarkEnabled -XX:ParallelGCThreads=2 $JAVA_OPTS "
+JAVA_OPTS="-Xms4G -Xmx4G $JAVA_OPTS " # java-memory-settings
+export JAVA_HOME
+export JRE_HOME
+export JAVA_OPTS
+#export LD_LIBRARY_PATH
