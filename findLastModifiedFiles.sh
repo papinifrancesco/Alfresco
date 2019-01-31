df94b2c091497a34000ca54769f8b68b197ff4b1
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2019-01-31 13:08:17 +0100

    Update findLastModifiedFiles.sh

--- findLastModifiedFiles.sh
+++ findLastModifiedFiles.sh
@@ -1,3 +1,4 @@
 clear
-find /soft_install/alfresco-5.1.2/ -type f -regextype posix-extended -iregex '.*.(jsp|orig|properties|sh|xml)' | grep -v "/temp/" > /root/file_list.txt
+find /opt/alfresco/ -type f -regextype posix-extended -iregex '.*.(jsp|orig|properties|sh|xml)' |\
+grep -E -v  '/temp/|[0-9].xml' > /root/file_list.txt
 while read -r line; do stat -c '%Y %n' "$line"; done < /root/file_list.txt | sort -n | cut -d ' ' -f2
