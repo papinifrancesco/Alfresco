fd37549db58eaed5c7683d9415bee30aab8740cd
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2019-04-19 11:06:33 +0200

    Update compareAlfrescoInstallations.sh

--- compareAlfrescoInstallations.sh
+++ compareAlfrescoInstallations.sh
@@ -4,5 +4,5 @@ NDIR=/opt/alfresco-5.2.4
 
 reset; reset; 
 diff -qr $ODIR $NDIR | \
-grep -E -v  '*BAK*|\.a|\.h|\.bat|\.bau|\.bin|\.cfg|\.class|\.css|.dat|.dtd|.desktop|.dic|examples|.gif|.glsl|.gz|.html|.jar|.java|.la|LibreLogo|.log|.m4|.mdl|.mod|.pc|.pl|.pdf|.png|.py|.rdb|.sample|.so|.sdg|.str|.svg|.thm|.ttf|.ui|templates|terminfo|.txt|.war|.xba|.xcd|.xlb|.xsl|.zip' | \
+grep -E -v  '*BAK*|\.a|\.h|\.bat|\.bau|\.bin|\.cfg|\.class|\.css|\.dat|\.dtd|\.desktop|\.dic|\examples|\.gif|\.glsl|\.gz|\.html|\.jar|\.java|\.la|LibreLogo|\.log|\.m4|\.mdl|\.mod|\.pc|\.pl|\.pdf|\.png|\.py|\.rdb|\.sample|\.so|\.sdg|\.str|\.svg|\.thm|\.ttf|\.ui|templates|terminfo|\.txt|\.war|\.xba|\.xcd|\.xlb|\.xsl|\.zip' | \
 sort ;
