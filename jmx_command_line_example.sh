2bf655c3282aa4ec27fc3aa648fe41669c0737b5
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2019-06-11 14:26:54 +0200

    Create jmx_command_line_example.sh

---
+++ jmx_command_line_example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# define the method to make a JMX connection first
+alf_jmx_connect ()
+{
+/opt/alfresco-one/java/bin/java -jar /opt/alfresco-one/scripts/jmxterm-1.0-alpha-4-uber.jar -l service:jmx:rmi:///jndi/rmi://localhost:50500/alfresco/jmxrmi -u controlRole -p change_asap -n
+}
+
+echo -n $(date +"%Y-%m-%d_%H-%M-%S")
+echo -n " ; "
+# pipe the commands for JMX to the method above
+echo -n 'get -b "Alfresco:Name=RepoServerMgmt" UserCountNonExpired' | alf_jmx_connect 2>&1 | grep UserCountNonExpired | tr '\n' ' '
+echo -n 'get -b "Alfresco:Name=ConnectionPool" NumActive' | alf_jmx_connect 2>&1 | grep NumActive | tr '\n' ' '
+echo -n 'get -b "Alfresco:Name=ConnectionPool" NumIdle'   | alf_jmx_connect 2>&1 | grep NumIdle
+
+# define a job in crontab
+# crontab -e
+# */5 * * * * /opt/alfresco-one/my_scripts/logged_sessions.sh >> /root/logged_sessions.out 2>&1
+#
+# and you have a basic way to log what you what with local JMX, lean and esay
