9c52f59ee41e1314a56fe3499a066854ff956bf7
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2019-06-25 14:58:30 +0200

    Update libreoffice_check.sh

--- libreoffice_check.sh
+++ libreoffice_check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# libreoffice_check.sh should be called by alfresco.sh , put a line below LIBREOFFICE_SCRIPT :
+# LIBREOFFICE_CHECK=$INSTALLDIR/libreoffice/scripts/libreoffice_check.sh
 
 pgrep -fl office -P 1 > /dev/null 2>&1
 #[ $? -eq 0 ] && echo "orphan LibreOffice process detected" || echo "no orphan LibreOffice process detected"
