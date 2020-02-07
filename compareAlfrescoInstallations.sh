d9587c897ecf6d43dd0ebf274b9707e92fde0811
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-02-07 11:15:43 +0100

    Update compareAlfrescoInstallations.sh

--- compareAlfrescoInstallations.sh
+++ compareAlfrescoInstallations.sh
@@ -1,22 +1,13 @@
+# cautious approach: since we're not completely sure about what   #
+# we really want, it is better to just exclude what we don't want #
 ##### do it for both /opt/alfresco /opt/solr #####
-ODIR=/opt/alfresco-one-5.1.2
-NDIR=/opt/alfresco-5.2.4
-FILTER="*BAK*|\.a|\.h|\.bat|\.bau|\.bin|\.cfg|\.class|\.css|\.dat|\.dtd|\.desktop|\.dic|\examples|\.gif|\.glsl|\.gz|\.html|\.jar|\.java|\.la|LibreLogo|\.log|\.m4|\.mdl|\.mod|\.pc|\.pl|\.pdf|\.png|\.py|\.rdb|\.sample|\.so|\.sdg|\.str|\.svg|\.thm|\.ttf|\.ui|templates|terminfo|\.txt|\.war|\.xba|\.xcd|\.xlb|\.xsl|\.zip"
-reset; reset; 
-diff -qrN $ODIR $NDIR | \
-grep -E -v  '$FILTER' | \
+ODIR=/ecm/software/alfresco/
+NDIR=/ecm/software/alfresco-content-services-6.2.0/
+FILTER_DIRS="webapps\/manager|webapps\/host-manager|tomcat\/logs|tomcat\/work|tomcat\/temp|libreoffice\/program|libreoffice/share"
+FILTER_EXTENSIONS="\.template|\.sample|\.ico|\.run|\.dll|\.1|\.ORIG|\.js|\.xsd|\.MF|\.a|\.h|\.bat|\.bau|\.bin|\.cfg|\.class|\.css|\.dat|\.dtd|\.desktop|\.dic|\.gif|\.glsl|\.gz|\.html|\.jar|\.java|\.la|\.log|\.m4|\.mdl|\.mod|\.pc|\.pl|\.pdf|\.png|\.py|\.rdb|\.sample|\.so|\.sdg|\.str|\.svg|\.thm|\.ttf|\.ui|\.txt|\.war|\.xba|\.xcd|\.xlb|\.xsl|\.zip"
+FILTER_STRINGS="*BAK*|CONTRIBUTING|RELEASE-NOTES|README|NOTICE|LICENSE|CREDITS|VERSIONS|examples|java|LibreLogo|templates|terminfo"
+FILTER="$FILTER_DIRS|$FILTER_EXTENSIONS|$FILTER_STRINGS"
+clean; clean;
+diff -qr $ODIR $NDIR | \
+grep -E -v $FILTER | \
 sort ;
-
-
-
-# different approach, maybe more clean
-
-reset;reset;
-diff -qrN $ODIR $NDIR |
-grep -E  '\.ORIG|\.pri|\.properties|\.pub|\.sh|\.xml' |
-grep -E -v '\.js|\.jar|\.jsp|\.class|\.war|\.gz|\.swf|\.java|\.ico|\.woff|\.svg|\.as|\.dvm|\.dim|\.tip|\.fdt|\.fdx|\.fnm|\.nvd|\.nvm|\.psd|/logs/' |
-grep -E -v '\.css|\.gif|\.jpg|\.html|\.png|\.sample|\.BAK|dashlets|site-webscripts|alfresco/messages|/cmm/|/documentlibrary/|/components/|example|maven\.org' |
-grep -E -v 'LibreLogo|google/docs|/soffice.cfg/|/liblangtag/|/nlpsolver/|/templates/|/libreoffice/share/extensions/|/libreoffice/share/wizards/' |
-grep -E -v '/alfrescoModels/|/js/lib/|/js/scripts/|/host-manager/|/manager/|/java/|/ImageMagick|/libreoffice/help/|/model/|/content/|/skins/' |
-sort;
-
