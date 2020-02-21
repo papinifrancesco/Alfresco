e1bc44b91213e16567103533f82cf2557ba90a27
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-02-21 16:31:47 +0100

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -5,7 +5,7 @@ JAVA_HOME=/opt/alfresco/java
 JRE_HOME=$JAVA_HOME
 JAVA_OPTS="-XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Djava.awt.headless=true -Dalfresco.home=/opt/alfresco -XX:ReservedCodeCacheSize=128m $JAVA_OPTS "
 JAVA_OPTS="-Dcom.sun.management.jmxremote -Dsun.security.ssl.allowUnsafeRenegotiation=true $JAVA_OPTS "
-JAVA_OPTS="-XX:NewRatio=2 -XX:+CMSParallelRemarkEnabled -XX:ParallelGCThreads=2 $JAVA_OPTS "
+JAVA_OPTS="-XX:NewRatio=2 -XX:+CMSParallelRemarkEnabled -XX:ParallelGCThreads=2 -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true $JAVA_OPTS "
 JAVA_OPTS="-Xms4G -Xmx4G $JAVA_OPTS " # java-memory-settings
 export JAVA_HOME
 export JRE_HOME
