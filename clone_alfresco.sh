58a958366ba11a6058a3caaf3b690e526254dd47
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-03-27 18:46:35 +0100

    Update and rename clone.sh to clone_alfresco.sh

---
+++ clone_alfresco.sh
@@ -0,0 +1,14 @@
+### The idea is: I have a working installation in a test machine and I want to clone ###
+### that installation to the production machine. Probably most of the settings will  ###
+### be OK so we can rsync test to production but we don't want to clone the content  ###
+### stores , the Solr indexes nor the temporary files                                ###
+
+#!/bin/bash
+
+SOURCE=/opt/alfresco-content-services-6.2.0/
+
+DESTINATION=user@host:/opt/alfresco-content-services-6.2.0/
+
+EXCLUSIONS=clone_alfresco_exclusions.txt
+
+rsync -avz --exclude-from $EXCLUSIONS $SOURCE $DESTINATION
