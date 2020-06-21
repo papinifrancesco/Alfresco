9b014db2a0f7c537bacb4ccad3ecb6f8895f470b
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-06-21 12:19:42 +0200

    Update compareAlfrescoInstallations.sh

--- compareAlfrescoInstallations.sh
+++ compareAlfrescoInstallations.sh
@@ -1,13 +1,22 @@
+# As of 21/06/2020 still a work in progress, use it with caution
 # cautious approach: since we're not completely sure about what   #
 # we really want, it is better to just exclude what we don't want #
 ##### do it for both /opt/alfresco /opt/solr #####
 # usage ./compareAlfrescoInstallations.sh /ACSfolder1/ /ACSfolder2/
-FILTER_DIRS="\/alfresco-aos-module|classes\/alfresco\/messages|site-webscripts\/org\/alfresco|webapps\/manager|webapps\/host-manager|tomcat\/logs|tomcat\/work|tomcat\/temp|libreoffice\/program|libreoffice/share"
-FILTER_EXTENSIONS="\.ftm|\.bak|\.template|\.sample|\.ico|\.run|\.dll|\.1|\.ORIG|\.js|\.xsd|\.MF|\.a|\.h|\.bat|\.bau|\.bin|\.cfg|\.class|\.css|\.dat|\.dtd|\.desktop|\.dic|\.gif|\.glsl|\.gz|\.html|\.jar|\.java|\.la|\.log|\.m4|\.mdl|\.mod|\.pc|\.pl|\.pdf|\.png|\.py|\.rdb|\.sample|\.so|\.sdg|\.str|\.svg|\.thm|\.ttf|\.ui|\.txt|\.war|\.xba|\.xcd|\.xlb|\.xsl|\.zip"
-FILTER_STRINGS="googledocs|*BAK*|CONTRIBUTING|RELEASE-NOTES|README|NOTICE|LICENSE|CREDITS|VERSIONS|examples|java|LibreLogo|templates|terminfo"
-FILTER_EXPRESSIONS="..\.properties"
-FILTER="$FILTER_DIRS|$FILTER_EXTENSIONS|$FILTER_STRINGS" #|$FILTER_EXPRESSIONS"
+#libreoffice\/program|libreoffice/share"
+
+FILTER_DIRS="webapps\/manager"
+FILTER_DIRS="$FILTER_DIRS|webapps\/host-manager"
+FILTER_DIRS="$FILTER_DIRS|tomcat\/logs"
+FILTER_DIRS="$FILTER_DIRS|tomcat\/temp"
+FILTER_DIRS="$FILTER_DIRS|tomcat\/work"
+FILTER_DIRS="$FILTER_DIRS|\/libreoffice\/"
+FILTER_DIRS="$FILTER_DIRS|\/java\/"
+#FILTER_EXTENSIONS="\.ftm|\.bak|\.template|\.sample|\.ico|\.run|\.dll|\.1|\.ORIG|\.js|\.xsd|\.MF|\.a|\.h|\.bat|\.bau|\.bin|\.cfg|\.class|\.css|\.dat|\.dtd|\.desktop|\.dic|\.gif|\.glsl|\.gz|\.html|\.jar|\.java|\.la|\.log|\.m4|\.mdl|\.mod|\.pc|\.pl|\.pdf|\.png|\.py|\.rdb|\.sample|\.so|\.sdg|\.str|\.svg|\.thm|\.ttf|\.ui|\.txt|\.war|\.xba|\.xcd|\.xlb|\.xsl|\.zip"
+#FILTER_STRINGS="googledocs|*BAK*|CONTRIBUTING|RELEASE-NOTES|README|NOTICE|LICENSE|CREDITS|VERSIONS|examples|java|LibreLogo|templates|terminfo"
+#FILTER_EXPRESSIONS="..\.properties"
+#FILTER="$FILTER_DIRS|$FILTER_EXTENSIONS|$FILTER_STRINGS" #|$FILTER_EXPRESSIONS"
 clear; clear;
 diff -qr $1 $2 | \
-grep -E -v $FILTER | \
+grep -E -v $FILTER_DIRS | \
 sort ;
