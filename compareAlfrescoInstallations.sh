1068aa01e384ab7d6f79fc3bb9fd67b351d1d7c0
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-06-21 16:10:17 +0200

    Update compareAlfrescoInstallations.sh

--- compareAlfrescoInstallations.sh
+++ compareAlfrescoInstallations.sh
@@ -16,9 +16,11 @@ FILTER_DIRS="$FILTER_DIRS|\/java\/"
 FILTER_DIRS="$FILTER_DIRS|\/WEB-INF\/lib" # no so sure about this one
 #FILTER_EXTENSIONS="\.ftm|\.bak|\.template|\.sample|\.ico|\.run|\.dll|\.1|\.ORIG|\.js|\.xsd|\.MF|\.a|\.h|\.bat|\.bau|\.bin|\.cfg|\.class|\.css|\.dat|\.dtd|\.desktop|\.dic|\.gif|\.glsl|\.gz|\.html|\.jar|\.java|\.la|\.log|\.m4|\.mdl|\.mod|\.pc|\.pl|\.pdf|\.png|\.py|\.rdb|\.sample|\.so|\.sdg|\.str|\.svg|\.thm|\.ttf|\.ui|\.txt|\.war|\.xba|\.xcd|\.xlb|\.xsl|\.zip"
 #FILTER_STRINGS="googledocs|*BAK*|CONTRIBUTING|RELEASE-NOTES|README|NOTICE|LICENSE|CREDITS|VERSIONS|examples|java|LibreLogo|templates|terminfo"
-#FILTER_EXPRESSIONS="tomcat-i18n-??\.jar"
+FILTER_EXPRESSIONS="tomcat-i18n-??\.jar"
+FILTER_EXPRESSIONS="$FILTER_EXPRESSIONS|tomcat-i18n-??\.jar"
 #FILTER="$FILTER_DIRS|$FILTER_EXTENSIONS|$FILTER_STRINGS" #|$FILTER_EXPRESSIONS"
 clear; clear;
 diff -qr $1 $2 | \
-grep -E -v $FILTER_DIRS | \
+grep -E -v '$FILTER_DIRS' | \
+grep -E -v '$FILTER_EXPRESSIONS' | \
 sort ;
