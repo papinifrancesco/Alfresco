66a1e9ec21361dfe12c4e302a90f655dea4e2832
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-07-01 19:00:21 +0200

    Update compareAlfrescoInstallations.sh

--- compareAlfrescoInstallations.sh
+++ compareAlfrescoInstallations.sh
@@ -1,27 +1,30 @@
-# As of 21/06/2020 still a work in progress, use it with caution
+# 2020-01-07
 # cautious approach: since we're not completely sure about what   #
 # we really want, it is better to just exclude what we don't want #
 ##### do it for both /opt/alfresco /opt/solr #####
-# usage ./compareAlfrescoInstallations.sh /ACSfolder1/ /ACSfolder2/
-#libreoffice\/program|libreoffice/share"
+# usage compareAlfrescoInstallations.sh /ACSfolder1/ /ACSfolder2/
+# tip: use real paths, not symlinks <- I need to further investigate this one
 
-FILTER_DIRS="webapps\/manager"
-FILTER_DIRS="$FILTER_DIRS|webapps\/host-manager"
-FILTER_DIRS="$FILTER_DIRS|tomcat\/lib"  # check manually for different database drivers
-FILTER_DIRS="$FILTER_DIRS|tomcat\/logs"
-FILTER_DIRS="$FILTER_DIRS|tomcat\/temp"
-FILTER_DIRS="$FILTER_DIRS|tomcat\/work"
-FILTER_DIRS="$FILTER_DIRS|\/libreoffice\/"
-FILTER_DIRS="$FILTER_DIRS|\/java\/"
-FILTER_DIRS="$FILTER_DIRS|\/WEB-INF\/lib" # no so sure about this one
-#FILTER_EXTENSIONS="\.ftm|\.bak|\.template|\.sample|\.ico|\.run|\.dll|\.1|\.ORIG|\.js|\.xsd|\.MF|\.a|\.h|\.bat|\.bau|\.bin|\.cfg|\.class|\.css|\.dat|\.dtd|\.desktop|\.dic|\.gif|\.glsl|\.gz|\.html|\.jar|\.java|\.la|\.log|\.m4|\.mdl|\.mod|\.pc|\.pl|\.pdf|\.png|\.py|\.rdb|\.sample|\.so|\.sdg|\.str|\.svg|\.thm|\.ttf|\.ui|\.txt|\.war|\.xba|\.xcd|\.xlb|\.xsl|\.zip"
-#FILTER_STRINGS="googledocs|*BAK*|CONTRIBUTING|RELEASE-NOTES|README|NOTICE|LICENSE|CREDITS|VERSIONS|examples|java|LibreLogo|templates|terminfo"
-FILTER_EXPRESSIONS="tomcat-i18n-??\.jar"
-FILTER_EXPRESSIONS="$FILTER_EXPRESSIONS|tomcat-i18n-??\.jar"
-FILTER_EXPRESSIONS="$FILTER_EXPRESSIONS|version\.properties"
-#FILTER="$FILTER_DIRS|$FILTER_EXTENSIONS|$FILTER_STRINGS" #|$FILTER_EXPRESSIONS"
+FILTER_DIRS="webapps\/manager\\b"
+FILTER_DIRS="$FILTER_DIRS|webapps\/host-manager\\b"
+FILTER_DIRS="$FILTER_DIRS|tomcat\/lib\\b"  # check manually for different database drivers
+FILTER_DIRS="$FILTER_DIRS|tomcat\/logs\\b"
+FILTER_DIRS="$FILTER_DIRS|tomcat\/temp\\b"
+FILTER_DIRS="$FILTER_DIRS|tomcat\/work\\b"
+FILTER_DIRS="$FILTER_DIRS|\/WEB-INF\/classes\/alfresco\/site-webscripts\\b"
+FILTER_DIRS="$FILTER_DIRS|\/libreoffice\\b"
+FILTER_DIRS="$FILTER_DIRS|\/java\\b"
+FILTER_DIRS="$FILTER_DIRS|\/WEB-INF\/lib\\b" # no so sure about this one
+FILTER_DIRS="$FILTER_DIRS|\/3rd-party\\b"
+#FILTER_EXTENSIONS="\.bak\\b|\.template\\b|\.sample\\b|\.ico\\b|\.run\\b|\.dll\\b|\.1\\b|\.ORIG\\b|\.js\\b|\.xsd\\b|\.MF\\b|\.a\\b|\.h\\b|\.bat\\b|\.bau\\b|\.bin\\b|\.cfg\\b|\.class\\b|\.css\\b|\.dat\\b|\.dtd\\b|\.desktop\\b|\.dic\\b|\.gif\\b|\.glsl\\b|\.gz\\b|\.html\\b|\.jar\\b|\.java\\b|\.la\\b|\.log\\b|\.m4\\b|\.mdl\\b|\.mod\\b|\.pc\\b|\.pl\\b|\.pdf\\b|\.png\\b|\.py\\b|\.rdb\\b|\.sample\\b|\.so\\b|\.sdg\\b|\.str\\b|\.svg\\b|\.thm\\b|\.ttf\\b|\.ui\\b|\.txt\\b|\.war\\b|\.xba\\b|\.xcd\\b|\.xlb\\b|\.xsl\\b|\.zip\\b"
+FILTER_EXTENSIONS="\.bin\\b|\.css\\b|\.gz\\b|\.install\\b|\.js\\b|\.sample\\b|\.ORIG\\b|\.bat\\b|\.war\\b|\.class\\b|\.svg\\b|\.xsd\\b|\.gif\\b"
+#FILTER_EXTENSIONS="$FILTER_EXTENSIONS|"
+FILTER_STRINGS="googledocs|*BAK*|CONTRIBUTING|RELEASE-NOTES|README|NOTICE|LICENSE|CREDITS|VERSIONS|examples|LibreLogo|templates|terminfo|cloud-folder|cloud-sync-management"
+FILTER_STRINGS="$FILTER_STRINGS|user-cloud-auth"
+FILTER_EXPRESSIONS="tomcat-i18n-??\.jar\\b|pom\.properties\\b|pom\.xml\\b|RUNNING\.txt\\b|MANIFEST\.MF\\b|BUILDING\.txt\\b|_??_??\.properties\\b"
+FILTER_EXPRESSIONS="$FILTER_EXPRESSIONS|version\.properties\\b"
+FILTER="$FILTER_DIRS|$FILTER_EXTENSIONS|$FILTER_STRINGS|$FILTER_EXPRESSIONS"
 clear; clear;
 diff -qr $1 $2 | \
-grep -E -v '$FILTER_DIRS' | \
-grep -E -v '$FILTER_EXPRESSIONS' | \
+grep -Ev "$FILTER" | \
 sort ;
