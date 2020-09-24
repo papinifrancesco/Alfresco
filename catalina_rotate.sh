75be6322c3016d196038482a54c55a3098b7fb29
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2020-09-24 13:17:57 +0200

    Update catalina_rotate.sh

--- catalina_rotate.sh
+++ catalina_rotate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# should be put in /usr/local/script
+# should be put in /usr/local/scripts
 
 DATE_F=`date +%F`
 TCAT_HOME=$1
@@ -7,6 +7,6 @@ TCAT_HOME=$1
 if [ -s $TCAT_HOME/logs/catalina.out ]; then
 cp -p $TCAT_HOME/logs/catalina.out $TCAT_HOME/logs/catalina_${DATE_F}.out
 cat /dev/null > $TCAT_HOME/logs/catalina.out
-gzip -9 $TCAT_HOME/logs/catalina_${DATE_F}.out
+xz -9 $TCAT_HOME/logs/catalina_${DATE_F}.out
 fi
 
