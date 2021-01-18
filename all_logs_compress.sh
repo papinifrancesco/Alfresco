cd25433e722c6d4c0967a347c7902b167c5d691a
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2021-01-18 15:54:24 +0100

    Update all_logs_compress.sh

--- all_logs_compress.sh
+++ all_logs_compress.sh
@@ -7,6 +7,6 @@ find "$TCAT_HOME/logs" -maxdepth 1 -type f -size 0 | while read -r filename ; do
 
 find "$TCAT_HOME/logs" -maxdepth 1 -type f -mmin +180 -exec bash -c "file {} | grep -q xz || /usr/sbin/lsof {} > /dev/null || [ ! -s {} ] || xz -9 {}" \;
 
-find "$TCAT_HOME/logs" -maxdepth 1 -type f -name "*.xz" -exec mv {} "$TCAT_HOME/logs/history/" \;
+find "$TCAT_HOME/logs" -maxdepth 1 -type f -name "*.xz" -exec mv {} "$TCAT_HOME/logs/old/" \;
 
-find "$TCAT_HOME/logs/history" -maxdepth 1 -mtime +1 -type f -name "*.xz" -exec rm -f {} \;
+find "$TCAT_HOME/logs/old/" -maxdepth 1 -mtime +180 -type f -name "*.xz" -exec rm -f {} \;
