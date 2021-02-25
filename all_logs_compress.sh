49dcbf69eb6cc549dfecb9251ef8d8c896847c0c
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2021-02-25 12:25:32 +0100

    Update all_logs_compress.sh

--- all_logs_compress.sh
+++ all_logs_compress.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# should be put in /usr/local/scripts
+# should be put in /opt/alfresco/scripts
 
-TCAT_HOME=$1
+LOGS=/opt/alfresco/tomcat/logs
 
-find "$TCAT_HOME/logs" -maxdepth 1 -type f -size 0 | while read -r filename ; do /sbin/fuser -s "$filename" || rm -f "$filename" ; done
+# delete 0 size files not in use
+find "$LOGS" -maxdepth 1 -type f -size 0 | while read -r filename ; do /sbin/fuser -s "$filename" || rm -f "$filename" ; done
 
-find "$TCAT_HOME/logs" -maxdepth 1 -type f -mmin +180 -exec bash -c "file {} | grep -q xz || /usr/sbin/lsof {} > /dev/null || [ ! -s {} ] || xz -9 {}" \;
+# if a log file is not in use, move it to old/
+find "$LOGS" -maxdepth 1 -type f | while read -r filename ; do /sbin/fuser -s "$filename" || mv "$filename" $LOGS/old/ ; done
 
-find "$TCAT_HOME/logs" -maxdepth 1 -type f -name "*.xz" -exec mv {} "$TCAT_HOME/logs/old/" \;
+# compress all files inside old/ that are not an .xz already
+find "$LOGS/old" -maxdepth 1 -type f \! -name "*.xz" | while read -r filename ; do /usr/bin/xz -9 "$filename" ; done
 
-find "$TCAT_HOME/logs/old/" -maxdepth 1 -mtime +180 -type f -name "*.xz" -exec rm -f {} \;
+# remove all compressed logs older than 180 days
+find "$LOGS/old" -maxdepth 1 -mtime +180 -type f -name "*.xz" -exec rm -f {} \;
