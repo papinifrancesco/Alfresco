b841dae73146b95aec43e3c8d8d6badeec6d90cd
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2023-01-24 13:16:43 +0100

    Update setenv.sh

--- tomcat/bin/setenv.sh
+++ tomcat/bin/setenv.sh
@@ -7,16 +7,34 @@ JAVA_HOME=/opt/alfresco/java
 
 JRE_HOME=$JAVA_HOME
 
-JAVA_OPTS="                                                                                     \
+JAVA_OPTS="                                                                                   # Load Tomcat Native Library
+LD_LIBRARY_PATH=/opt/alfresco/tomcat/lib:$LD_LIBRARY_PATH
+
+CATALINA_PID=/opt/alfresco/tomcat/temp/catalina.pid
+
+JAVA_HOME=/opt/alfresco/java
+
+JRE_HOME=$JAVA_HOME
+
+JAVA_OPTS="$JAVA_OPTS                                                                           \
            -Dalfresco.home=/opt/alfresco                                                        \
            -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true                        \
            -Dcom.sun.management.jmxremote                                                       \
            -Djava.awt.headless=true                                                             \
+           -Dencryption.keystore.type=PKCS12                                                    \
+           -Dencryption.keystore.provider=SUN                                                   \
+           -Dencryption.cipherAlgorithm=AES                                                     \
+           -Dencryption.keyAlgorithm=AES                                                        \
+           -Dencryption.keystore.location=/opt/alfresco/alf_data/keystore/keystore              \
+           -Dmetadata-keystore.password=password                                                \
+           -Dmetadata-keystore.aliases=metadata                                                 \
+           -Dmetadata-keystore.metadata.password=password                                       \
+           -Dmetadata-keystore.metadata.algorithm=AES                                           \
            -Djavax.net.ssl.keyStore=/opt/alfresco/alf_data/keystore/ssl.keystore                \
-           -Djavax.net.ssl.keyStorePassword=kT9X6oe68t                                          \
+           -Djavax.net.ssl.keyStorePassword=keystore                                            \
            -Djavax.net.ssl.keyStoreType=JCEKS                                                   \
            -Djavax.net.ssl.trustStore=/opt/alfresco/alf_data/keystore/ssl.truststore            \
-           -Djavax.net.ssl.trustStorePassword=kT9X6oe68t                                        \
+           -Djavax.net.ssl.trustStorePassword=truststore                                        \
            -Djavax.net.ssl.trustStoreType=JCEKS                                                 \
            -XX:+CMSParallelRemarkEnabled                                                        \
            -XX:+DisableExplicitGC                                                               \
@@ -27,12 +45,11 @@ JAVA_OPTS="                                                                     
            -Xdebug                                                                              \
            -Xlog:gc*                                                                            \
            -Xlog:gc:/opt/alfresco/tomcat/logs/gc.log                                            \
-           -Xms4G                                                                              \
-           -Xmx4G                                                                              \
+           -Xms4G                                                                               \
+           -Xmx4G                                                                               \
            -Xrunjdwp:transport=dt_socket,address=*:8000,server=y,suspend=n                      \
            -verbose:gc                                                                          \
                                                                                               " ;
-
 export CATALINA_PID
 
 export JAVA_HOME
