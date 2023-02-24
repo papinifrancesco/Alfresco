c0e9d5cab03a3ad6ba4ab57be1dfd6eb47ed6510
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2023-02-24 15:58:43 +0100

    Update alfresco-shared-file-start.sh
    cosmetics

--- opt/alfresco/scripts/alfresco-shared-file-start.sh
+++ opt/alfresco/scripts/alfresco-shared-file-start.sh
@@ -5,16 +5,16 @@ WDIR=$TS
 FILE=alfresco-shared-file-store-controller
 PATTERN=shared-file-store
 
-JAVA_OPTS="$JAVA_OPTS \
-  -Xmx1g \
-  -Dspring.servlet.multipart.max-file-size=8192MB \
+JAVA_OPTS="$JAVA_OPTS                                \
+  -Xmx1g                                             \
+  -Dspring.servlet.multipart.max-file-size=8192MB    \
   -Dspring.servlet.multipart.max-request-size=8192MB \
-  -Dserver.port=8099 \
-  -Dserver.error.include-message=ALWAYS \
-  -Dlogging.level.org.alfresco.store=warn \
-  -DfileStorePath="$TS/tmp/Alfresco" \
+  -Dserver.port=8099                                 \
+  -Dserver.error.include-message=ALWAYS              \
+  -Dlogging.level.org.alfresco.store=warn            \
+  -DfileStorePath="$TS/tmp/Alfresco"                 \
   -Dscheduler.contract.path="$TS/tmp/scheduler.json" \
-  -Dscheduler.content.age.millis=900000 \
-  -Dscheduler.cleanup.interval=900000"
+  -Dscheduler.content.age.millis=900000              \
+  -Dscheduler.cleanup.interval=900000"               ;
 
 /opt/alfresco/java/bin/java ${JAVA_OPTS} -jar $FILE >> $WDIR/logs/$PATTERN.out 2>> $WDIR/logs/$PATTERN.err &
