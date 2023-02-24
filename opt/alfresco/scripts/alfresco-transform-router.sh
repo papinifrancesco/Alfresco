57b4b7d28bf4fd89748ee642ba1c21da80ac1c40
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2023-02-24 16:52:13 +0100

    Update alfresco-transform-router.sh

--- opt/alfresco/scripts/alfresco-transform-router.sh
+++ opt/alfresco/scripts/alfresco-transform-router.sh
@@ -5,11 +5,11 @@ WDIR=$TS
 FILE=alfresco-transform-router
 PATTERN=transform-router
 
-JAVA_OPTS="$JAVA_OPTS \
-  -Xmx1g \
-  -DCORE_AIO_URL=http://localhost:8090 \
-  -DCORE_AIO_QUEUE=org.alfresco.transform.engine.aio.acs \
+JAVA_OPTS="$JAVA_OPTS                                                                       \
+  -Xmx1g                                                                                    \
+  -DCORE_AIO_URL=http://localhost:8090                                                      \
+  -DCORE_AIO_QUEUE=org.alfresco.transform.engine.aio.acs                                    \
   -DFILE_STORE_URL=http://localhost:8099/alfresco/api/-default-/private/sfs/versions/1/file \
-  -DACTIVEMQ_URL=failover:(tcp://127.0.0.1:61616)?timeout=3000"
+  -DACTIVEMQ_URL=failover:(tcp://127.0.0.1:61616)?timeout=3000"                             ;
 
 /opt/alfresco/java/bin/java ${JAVA_OPTS} -jar $FILE >> $WDIR/logs/$PATTERN.out 2>> $WDIR/logs/$PATTERN.err &
