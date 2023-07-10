0ec1743106e83ee5903648bc2c6006ad201d84b2
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2023-07-10 12:46:16 +0200

    Update dp.sh

--- miscellaneous/dp.sh
+++ miscellaneous/dp.sh
@@ -4,9 +4,9 @@
 sed '/^#/ d' < "$1" > /tmp/temp1.txt
 sed '/^#/ d' < "$2" > /tmp/temp2.txt
 
-sort /tmp/temp1.txt
-sort /tmp/temp2.txt
+sort /tmp/temp1.txt > /tmp/temp3.txt
+sort /tmp/temp2.txt > /tmp/temp4.txt
 
-diff -y -W220 /tmp/temp1.txt /tmp/temp2.txt
+diff -y -W220 /tmp/temp3.txt /tmp/temp4.txt
 
-rm -f /tmp/temp1.txt /tmp/temp2.txt
+rm -f /tmp/temp3.txt /tmp/temp4.txt
