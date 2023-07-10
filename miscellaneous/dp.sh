0cd1ce70d3bac13ea9df83d8f704d81873ab560f
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2023-07-10 12:55:12 +0200

    Update dp.sh

--- miscellaneous/dp.sh
+++ miscellaneous/dp.sh
@@ -1,8 +1,8 @@
 # diff properties files
 #!/bin/bash
 
-sed '/^#/ d' < "$1" > /tmp/temp1.txt
-sed '/^#/ d' < "$2" > /tmp/temp2.txt
+sed '/^#/d' < "$1" > /tmp/temp1.txt
+sed '/^#/d' < "$2" > /tmp/temp2.txt
 
 sort /tmp/temp1.txt > /tmp/temp3.txt
 sort /tmp/temp2.txt > /tmp/temp4.txt
