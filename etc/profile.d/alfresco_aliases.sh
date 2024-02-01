01ca3f1c52daa335840243eea61e9b81ca35d52e
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-02-01 21:41:46 +0100

    Create alfresco_aliases.sh

---
+++ etc/profile.d/alfresco_aliases.sh
@@ -0,0 +1,6 @@
+
+alias AGP='vim /opt/alfresco/tomcat/shared/classes/alfresco-global.properties'
+
+alias COUT='less /opt/alfresco/tomcat/logs/catalina.out'
+
+alias MMT='/opt/alfresco/java/bin/java -jar /opt/alfresco/bin/alfresco-mmt.jar'
