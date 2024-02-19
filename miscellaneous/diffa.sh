205befbcd6474ba8bbc95cbf224ffe11f30dc01d
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-02-19 17:37:43 +0100

    Add files via upload

---
+++ miscellaneous/diffa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sed '/^#/ d' < "$1" > /tmp/temp1.txt
+sed '/^#/ d' < "$2" > /tmp/temp2.txt
+
+sort /tmp/temp1.txt > /tmp/temp3.txt
+sort /tmp/temp2.txt > /tmp/temp4.txt
+
+diff -y -W220 /tmp/temp3.txt /tmp/temp4.txt
+
+rm -f /tmp/temp3.txt /tmp/temp4.txt
