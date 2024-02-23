e1d2805d04881fc1319d6fcb06fb5bcdae834646
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-02-23 12:24:59 +0100

    Update clean-contentstore.deleted.sh

--- opt/alfresco/scripts/clean-contentstore.deleted.sh
+++ opt/alfresco/scripts/clean-contentstore.deleted.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# should be put in /opt/alfresco/scripts
+# crontab -e -u alfresco
+# 50 23 * * * /opt/alfresco/scripts/clean-contentstore.deleted.sh
+
 # Let's have a look, manually, at how much space we could reclaim:
 # find /opt/alf_data/contentstore.deleted/ -mtime +20 -type f -exec du -c {} + | awk '/total/{sum += $1} END {print sum/1048576}'
 
