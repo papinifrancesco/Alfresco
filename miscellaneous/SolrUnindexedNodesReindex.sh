5a57dfbad5a3edbd170331a29b4dc23b6738bfd0
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-08-01 18:56:50 +0200

    Update SolrUnindexedNodesReindex.sh

--- miscellaneous/SolrUnindexedNodesReindex.sh
+++ miscellaneous/SolrUnindexedNodesReindex.sh
@@ -7,3 +7,7 @@ tail -n +2 DBIDs.txt | while read -r DBID; do
   curl --header "x-alfresco-search-secret: secret" "http://127.0.0.1:8983/solr/admin/cores?action=REINDEX&nodeid=$DBID"
 
 done
+
+# In case we want to check what's going on, set these from Solr's GUI
+# log4j.logger.org.alfresco.solr.SolrInformationServer=DEBUG
+# log4j.logger.org.alfresco.solr.tracker.MetadataTracker=DEBUG
