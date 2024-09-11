682d7132dbac13d803bd7e82916af5108cba3889
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-09-11 19:04:20 +0200

    Update nodeRef-DBID-reindex_MySQL.sh

--- SOLR/nodeRef-DBID-reindex/nodeRef-DBID-reindex_MySQL.sh
+++ SOLR/nodeRef-DBID-reindex/nodeRef-DBID-reindex_MySQL.sh
@@ -1,5 +1,5 @@
 # We're given a report.json file with the nodeRef identified as "id" , first we have to clean it
-python3 ./filter.py3 > noderRef.txt
+python3 ./filter.py > noderRef.txt
 
 # We have to get the DBID from the DB (MySQL in this case)
 ./nodeRef-DBID.sh 2>/dev/null > DBID.txt
