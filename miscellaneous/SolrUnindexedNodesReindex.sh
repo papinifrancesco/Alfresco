9b7373d965e6a78c8c433567e7a3062811c7fa28
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-09-11 18:49:11 +0200

    Update SolrUnindexedNodesReindex.sh

--- miscellaneous/SolrUnindexedNodesReindex.sh
+++ miscellaneous/SolrUnindexedNodesReindex.sh
@@ -1,10 +1,18 @@
-curl -s --header "x-alfresco-search-secret: secret" "http://127.0.0.1:8983/solr/alfresco/afts?q=DOC_TYPE:UnindexedNode&rows=9999999&wt=csv&fl=DBID&cache=false&omitHeader=true" -o DBIDs.txt
+HEADER=x-alfresco-search-secret
+
+URL=http://127.0.0.1:8983/solr/admin/cores
+
+SECRET=secret
+
+
+curl -s --header "$HEADER: $SECRET" "http://127.0.0.1:8983/solr/alfresco/afts?q=DOC_TYPE:UnindexedNode&rows=9999999&wt=csv&fl=DBID&cache=false&omitHeader=true" -o DBIDs.txt
+
 
 tail -n +2 DBIDs.txt | while read -r DBID; do
 
-  curl --header "x-alfresco-search-secret: secret" "http://127.0.0.1:8983/solr/admin/cores?action=PURGE&nodeid=$DBID"
+  curl --header "$HEADER: $SECRET" "$URL?action=PURGE&nodeid=$DBID"
 
-  curl --header "x-alfresco-search-secret: secret" "http://127.0.0.1:8983/solr/admin/cores?action=REINDEX&nodeid=$DBID"
+  curl --header "$HEADER: $SECRET" "$URL?action=REINDEX&nodeid=$DBID"
 
 done
 
