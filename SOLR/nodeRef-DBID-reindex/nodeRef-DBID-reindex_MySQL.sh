bf9d877f917697a20e2ba7a0f2998e2105b0872e
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-09-11 18:55:26 +0200

    Create nodeRef-DBID-reindex_MySQL.sh

---
+++ SOLR/nodeRef-DBID-reindex/nodeRef-DBID-reindex_MySQL.sh
@@ -0,0 +1,8 @@
+# We're given a report.json file with the nodeRef identified as "id" , first we have to clean it
+python3 ./filter.py3 > noderRef.txt
+
+# We have to get the DBID from the DB (MySQL in this case)
+./nodeRef-DBID.sh 2>/dev/null > DBID.txt
+
+# We send SOLR a PURGE and REINDEX action for each DBID
+./reindex_nodes.sh
