f80759959c9b690281f15c2c1b273110f10ab133
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-10-16 14:59:32 +0200

    Update probes.sh

--- Monitoring/probes.sh
+++ Monitoring/probes.sh
@@ -25,9 +25,4 @@ curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/
 
 EPOCH="$(date +%s)" ; curl -s -H "x-alfresco-search-secret: secret" "http://172.30.31.57:8983/solr/alfresco/admin/ping?_=$EPOCH&ts=$EPOCH&wt=json' | jq
   
-  
-
-
-
 curl -s --header "x-alfresco-search-secret: secret" -X GET "http://172.30.31.57:8983/solr/alfresco/afts?q=DOC_TYPE:ErrorNode&wt=json" | jq 
-URL=
