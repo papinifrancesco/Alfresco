933fbe7b46fa50df84f33bb93911a7139d26a642
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-10-16 14:00:12 +0200

    Create probes.sh

---
+++ Monitoring/probes.sh
@@ -0,0 +1,21 @@
+
+
+
+
+
+curl -s -X GET "http://172.30.31.51:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
+
+curl -s -X GET "http://172.30.31.51:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-live-"  -H  "accept: application/json"
+
+
+
+curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
+
+curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-live-"  -H  "accept: application/json"
+
+
+
+curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
+
+curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-live-"  -H  "accept: application/json"
+
