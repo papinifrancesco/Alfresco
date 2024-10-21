1a75149c142265daf1545c550fec452604b38dff
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2024-10-21 16:53:19 +0200

    Update probes.sh

--- Monitoring/probes.sh
+++ Monitoring/probes.sh
@@ -4,22 +4,17 @@
 
 #########  ACS  #########
 
-curl -s -X GET "http://172.30.31.51:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
+curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
+curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-live-"  -H  "accept: application/json"
 
+curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
+curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-live-"  -H  "accept: application/json"
+
+curl -s -X GET "http://172.30.31.51:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
 curl -s -X GET "http://172.30.31.51:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-live-"  -H  "accept: application/json"
 
-
-curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
-
-curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-live-"  -H  "accept: application/json"
-
-
-
-curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
-
-curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/versions/1/probes/-live-"  -H  "accept: application/json"
-
 #########  ACS  #########
+
 
 
 
@@ -29,9 +24,11 @@ curl -s -X GET "http://172.30.31.52:8080/alfresco/api/-default-/public/alfresco/
 # brokerName has to be defined in:
 # /opt/activemq/conf/activemq.xml
 
-curl --noproxy '*' -u user:Apassword -s http://127.0.0.1:8161/api/jolokia/exec/org.apache.activemq:type=Broker,brokerName=edoc-activemq,service=Health/healthStatus | jq -r '.value'
+curl --noproxy '*' -u user:Apassword -s http://172.30.31.55:8161/api/jolokia/exec/org.apache.activemq:type=Broker,brokerName=edoc-activemq,service=Health/healthStatus | jq -r '.value'
+curl --noproxy '*' -u user:Apassword -s http://172.30.31.56:8161/api/jolokia/exec/org.apache.activemq:type=Broker,brokerName=edoc-activemq,service=Health/healthStatus | jq -r '.value'
 
 #########  ActiveMQ #########
+
 
 
 
@@ -55,19 +52,20 @@ curl --noproxy '*' -s -X GET "http://172.30.31.55:8097/live"
 curl --noproxy '*' -s -X GET "http://172.30.31.56:8097/live"
 curl --noproxy '*' -s -X GET "http://172.30.31.51:8097/live"
 
-
-
-
-
 #########  ATS #########
+
+
+
 
 
 #########  SOLR  #########
 
-EPOCH="$(date +%s)" ; curl -s -H "x-alfresco-search-secret: secret" "http://172.30.31.57:8983/solr/alfresco/admin/ping?_=$EPOCH&ts=$EPOCH&wt=json' | jq
+curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.57:8983/solr/alfresco/admin/ping?&wt=json" | jq
+curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.58:8983/solr/alfresco/admin/ping?&wt=json" | jq
+curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.51:8983/solr/alfresco/admin/ping?&wt=json" | jq
   
-curl -s --header "x-alfresco-search-secret: secret" -X GET "http://172.30.31.57:8983/solr/alfresco/afts?q=DOC_TYPE:ErrorNode&wt=json" | jq 
+curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.57:8983/solr/alfresco/afts?q=DOC_TYPE:ErrorNode&wt=json" | jq 
 
-curl --noproxy '*' -s --header "x-alfresco-search-secret: secret" -X GET "http://172.30.31.57:8983/solr/alfresco/afts?q=DOC_TYPE:ErrorNode&wt=json" | jq '{lastIndexedTxTime, txRemaining, numFound: .response.numFound}'
+curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.57:8983/solr/alfresco/afts?q=DOC_TYPE:ErrorNode&wt=json" | jq '{lastIndexedTxTime, txRemaining, numFound: .response.numFound}'
 
 #########  SOLR  #########
