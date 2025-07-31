497ded76dd9be8566ad3a054d0dd40c48168ae65
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2025-07-31 11:08:57 +0200

    Update probes.sh

--- Monitoring/probes.sh
+++ Monitoring/probes.sh
@@ -1,14 +1,13 @@
-# TAKE NOTES FIRST
-# REORGANISE LATER
+# only BASIC checks here!
+# To convert and to adapt to your monitoring solution such as
+# Nagios, Prometeus, Xymon, Zabbix etc. is up to you 
 
 
 #########  ACS  #########
-
 REPO="http://172.30.31.52:8080"
 
 curl -s -X GET "$REPO/alfresco/api/-default-/public/alfresco/versions/1/probes/-ready-" -H  "accept: application/json"
 curl -s -X GET "$REPO/alfresco/api/-default-/public/alfresco/versions/1/probes/-live-"  -H  "accept: application/json"
-
 #########  ACS  #########
 
 
@@ -16,14 +15,12 @@ curl -s -X GET "$REPO/alfresco/api/-default-/public/alfresco/versions/1/probes/-
 
 
 #########  ActiveMQ #########
-
 # brokerName has to be defined in:
 # /opt/activemq/conf/activemq.xml
 
 AMQ="http://172.30.31.55:8161"
 
 curl --noproxy '*' -u user:Apassword -s $AMQ/api/jolokia/exec/org.apache.activemq:type=Broker,brokerName=edoc-activemq,service=Health/healthStatus | jq -r '.value'
-
 #########  ActiveMQ #########
 
 
@@ -43,7 +40,6 @@ curl --noproxy '*' -s -X GET "$SFS/live"
 # CaDES is a custom transformer : you don't have it
 CDS="http://172.30.31.55:8097"
 curl --noproxy '*' -s -X GET "$CDS/live"
-
 #########  ATS #########
 
 
@@ -51,13 +47,9 @@ curl --noproxy '*' -s -X GET "$CDS/live"
 
 
 #########  SOLR  #########
+ASS="http://172.30.31.57:8983"
 
-curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.57:8983/solr/alfresco/admin/ping?&wt=json" | jq
-curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.58:8983/solr/alfresco/admin/ping?&wt=json" | jq
-curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.51:8983/solr/alfresco/admin/ping?&wt=json" | jq
-  
-curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.57:8983/solr/alfresco/afts?q=DOC_TYPE:ErrorNode&wt=json" | jq 
-
-curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "http://172.30.31.57:8983/solr/alfresco/afts?q=DOC_TYPE:ErrorNode&wt=json" | jq '{lastIndexedTxTime, txRemaining, numFound: .response.numFound}'
-
+curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "$ASS/solr/alfresco/admin/ping?&wt=json" | jq
+curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "$ASS/solr/alfresco/afts?q=DOC_TYPE:ErrorNode&wt=json" | jq 
+curl --noproxy '*' -s -H "x-alfresco-search-secret: secret" -X GET "$ASS/solr/alfresco/afts?q=DOC_TYPE:ErrorNode&wt=json" | jq '{lastIndexedTxTime, txRemaining, numFound: .response.numFound}'
 #########  SOLR  #########
