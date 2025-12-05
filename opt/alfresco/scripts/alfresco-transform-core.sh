90e7be12edcd7bc9ff53c04157c3d99b6e677a4a
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2025-12-05 17:37:17 +0100

    Update ActiveMQ URL in alfresco-transform-core.sh

--- opt/alfresco/scripts/alfresco-transform-core.sh
+++ opt/alfresco/scripts/alfresco-transform-core.sh
@@ -21,7 +21,7 @@ JAVA_OPTS="$JAVA_OPTS                                                           
   -DIMAGEMAGICK_DYN=/usr/lib64/ImageMagick-7.1.0/lib                                         \
   -DIMAGEMAGICK_EXE=/usr/bin/convert                                                         \
   -DPDFBOX_NOTEXTRACTBOOKMARKS_DEFAULT=false                                                 \
-  -DACTIVEMQ_URL=failover:(tcp://127.0.0.1:61616)?timeout=3000                               \
+  -DACTIVEMQ_URL=failover:(tcp://10.64.112.40:61616,tcp://10.64.112.41:61616)?timeout=3000   \
   -DFILE_STORE_URL=http://localhost:8099/alfresco/api/-default-/private/sfs/versions/1/file" ;
 
 /opt/alfresco/java/bin/java ${JAVA_OPTS} -jar $FILE > $TS/logs/$PATTERN.out 2> $TS/logs/$PATTERN.err &
