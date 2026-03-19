7f63fdeacff691b3b096c638194caf3af2dd3265
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2026-03-19 10:22:45 +0100

    Adjust JAVA_OPTS and update log file paths

--- opt/alfresco/scripts/alfresco-shared-file-start.sh
+++ opt/alfresco/scripts/alfresco-shared-file-start.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-
 TS=/opt/alfresco/transform-service
+WDIR=$TS
 
 FILE=alfresco-shared-file-store-controller
-
 PATTERN=shared-file-store
 
-JAVA_OPTS="$JAVA_OPTS                                \
-  -Xms1g                                             \
-  -Xmx1g                                             \
-  -Dspring.servlet.multipart.max-file-size=8192MB    \
-  -Dspring.servlet.multipart.max-request-size=8192MB \
-  -Dserver.port=8099                                 \
-  -Dserver.error.include-message=ALWAYS              \
-  -Dlogging.level.org.alfresco.store=warn            \
-  -DfileStorePath="$TS"/tmp/Alfresco                 \
-  -Dscheduler.contract.path="$TS"/tmp/scheduler.json \
-  -Dscheduler.content.age.millis=900000              \
-  -Dscheduler.cleanup.interval=900000"               ;
+JAVA_OPTS="$JAVA_OPTS                                                          \
+  -Xms512m                                                                     \
+  -Xmx512m                                                                     \
+  -XX:+ExitOnOutOfMemoryError                                                  \
+  -Dspring.servlet.multipart.max-file-size=8192MB                              \
+  -Dspring.servlet.multipart.max-request-size=8192MB                           \
+  -Dserver.port=8099                                                           \
+  -Dserver.error.include-message=ALWAYS                                        \
+  -Dlogging.level.org.alfresco.store=warn                                      \
+  -DfileStorePath=/opt/alfresco/transform-service/tmp/Alfresco                 \
+  -Dscheduler.contract.path=/opt/alfresco/transform-service/tmp/scheduler.json \
+  -Dscheduler.content.age.millis=900000                                        \
+  -Dscheduler.cleanup.interval=900000"                                         ;
 
-/opt/alfresco/java/bin/java ${JAVA_OPTS} -jar $FILE >> $TS/logs/$PATTERN.out 2>> $TS/logs/$PATTERN.err &
+/opt/alfresco/java/bin/java ${JAVA_OPTS} -jar $FILE > $WDIR/logs/$PATTERN.out 2> $WDIR/logs/$PATTERN.err &
+echo $! > $WDIR/shared-file-store.pid
