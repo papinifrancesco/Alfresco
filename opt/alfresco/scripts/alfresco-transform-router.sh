18522dc03309d5bc03b1d17e641cfa3b2f59beeb
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2026-03-31 16:09:22 +0200

    Increase Java heap size and add temp directory option

--- opt/alfresco/scripts/alfresco-transform-router.sh
+++ opt/alfresco/scripts/alfresco-transform-router.sh
@@ -5,13 +5,13 @@ FILE=alfresco-transform-router
 PATTERN=transform-router
 
 JAVA_OPTS="$JAVA_OPTS                                                                       \
-  -Xms512m                                                                                  \
-  -Xmx512m                                                                                  \
+  -Xmx2048m                                                                                 \
   -XX:+ExitOnOutOfMemoryError                                                               \
   -DCORE_AIO_URL=http://127.0.0.1:8090                                                      \
   -DCORE_AIO_QUEUE=org.alfresco.transform.engine.aio.acs                                    \
   -DFILE_STORE_URL=http://127.0.0.1:8099/alfresco/api/-default-/private/sfs/versions/1/file \
-  -DACTIVEMQ_URL=failover:(tcp://127.0.0.1:61616)?timeout=3000"                             ;
+  -DACTIVEMQ_URL=failover:(tcp://127.0.0.1:61616)?timeout=3000                              \
+  -Djava.io.tmpdir="$TS"/tmp"                                                               ;
 
 /opt/alfresco/java/bin/java ${JAVA_OPTS} -jar $FILE > $TS/logs/$PATTERN.out 2> $TS/logs/$PATTERN.err &
 echo $! > $TS/transform-router.pid
