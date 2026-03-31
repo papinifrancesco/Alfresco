55b7e175cc3f5b184956a2ad76beb471686c0f97
Francesco Papini <37743983+papinifrancesco@users.noreply.github.com>
2026-03-31 16:12:16 +0200

    Update Java options and ImageMagick paths

--- opt/alfresco/scripts/alfresco-transform-core.sh
+++ opt/alfresco/scripts/alfresco-transform-core.sh
@@ -8,8 +8,7 @@ FILE=alfresco-transform-core-aio
 PATTERN=transform-core-aio
 
 JAVA_OPTS="$JAVA_OPTS                                                                           \
-  -Xms2G                                                                                        \
-  -Xmx4G                                                                                        \
+  -Xmx12g                                                                                        \
   -XX:+ExitOnOutOfMemoryError                                                                   \
   -DTRANSFORM_ENGINE_REQUEST_QUEUE=org.alfresco.transform.engine.aio.acs                        \
   -DPDFRENDERER_EXE=/opt/alfresco/transform-service/alfresco-pdf-renderer/alfresco-pdf-renderer \
@@ -18,12 +17,13 @@ JAVA_OPTS="$JAVA_OPTS                                                           
   -DLIBREOFFICE_TIMEOUT=120000                                                                  \
   -DLIBREOFFICE_PORT_NUMBERS=8100                                                               \
   -DLIBREOFFICE_IS_ENABLED=true                                                                 \
-  -DIMAGEMAGICK_ROOT=/usr/lib64/ImageMagick-7.1.0                                               \
-  -DIMAGEMAGICK_DYN=/usr/lib64/ImageMagick-7.1.0/lib                                            \
+  -DIMAGEMAGICK_ROOT=/usr/lib64/ImageMagick-7.0.7                                               \
   -DIMAGEMAGICK_EXE=/usr/bin/convert                                                            \
   -DPDFBOX_NOTEXTRACTBOOKMARKS_DEFAULT=false                                                    \
   -DACTIVEMQ_URL=failover:(tcp://127.0.0.1:61616)?timeout=3000                                  \
-  -DFILE_STORE_URL=http://127.0.0.1:8099/alfresco/api/-default-/private/sfs/versions/1/file"    ;
+  -DFILE_STORE_URL=http://127.0.0.1:8099/alfresco/api/-default-/private/sfs/versions/1/file     \
+  -Dlogging.level.org.alfresco.transform.router.TransformerDebug=INFO                           \
+  -Djava.io.tmpdir="$TS"/tmp                                                                    "
 
 /opt/alfresco/java/bin/java ${JAVA_OPTS} -jar $FILE > $WDIR/logs/$PATTERN.out 2> $WDIR/logs/$PATTERN.err &
 echo $! > $WDIR/transform-core-aio.pid
